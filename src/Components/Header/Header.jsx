cd5a1ffbb871605615e2126050d0310374d96d7c
Sumit kumar <cflip3913@gmail.com>
2024-07-01 00:32:38 +0530

    complete header part and complete home page

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,28 @@
- 
+import InstagramIcon from './instagram.png'
+import YoutubeIcon from './youtube.png'
+import TelegramIcon from './telegram.png'
+import './Header.css'
 function Header() {
   return (
-    <div>
-        
-    </div>
+    <header>
+      <div className="social-media-icons">
+        <img src={InstagramIcon} alt="" />
+        <img src={YoutubeIcon} alt="" />
+        <img src={TelegramIcon} alt="" />
+      </div>
+      <hr />
+      <div className="hearder-nav">
+        <div className="header-nav-items">
+          <div className="header-nav-item">ABOUT US</div>
+          <div className="header-nav-item">PRIVACY POLICY</div>
+          <div className="header-nav-item">REFUNDS & CANCELLATIONS POLICY</div>
+          <div className="header-nav-item">CONTACT US</div>
+        </div>
+        <div className="tradeMark">
+          Copyright &#169; 2023 - 2024 GREYHATSHADOW
+        </div>
+      </div>
+    </header>
   )
 }
 
